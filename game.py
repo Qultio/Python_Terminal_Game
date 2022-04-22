a88b54d09b189156cbd3f3147c083fde79d35d74
Qultio <Benjaminneyt@gmail.com>
2022-04-22 08:35:31 +0200

    Explained what I try to do

---
+++ game.py
@@ -0,0 +1,17 @@
+"""
+Welcome to my first python project!
+
+This is made with the help of the Codecademy team!
+Great site ! Does guys know what they are doing ;) 
+
+This is an simple quiz game. Don't judge me to hard :D 
+
+Start of the project: 22/04/2022
+
+--------------------------------------------------------------------------------------------------
+What is this game?
+
+A text- based adventure in a fantasy world ! 
+It's like a short D&D thing :) 
+
+"""
