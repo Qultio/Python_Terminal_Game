fd403e37ec8eb4466a0ace78e4bb5be28c128f7d
Qultio <Benjaminneyt@gmail.com>
2022-04-23 09:19:00 +0200

    Made entry point and make a Player Character

--- game.py
+++ game.py
@@ -15,3 +15,45 @@ A text- based adventure in a fantasy world !
 It's like a short D&D thing :) 
 
 """
+# imports
+import keyboard
+
+# Classes
+class Player:
+    def __init__(self, name, race, class_player, description):
+        self.race = race
+        self.class_player = class_player
+        self.description = description
+        self.name = name
+
+
+# Functions
+def makePlayer():
+    print("Welcome, brave adventurer !")
+    print("Can you type your name for me?")
+    name = input()
+    print("What race are you?")
+    print("Choose between \"Dragonborn\", \"Elf\", \"Dwarf\" or \"Human\"")
+    race = input()
+    print("Which Class do you want to be?")
+    print("Choose between \"Paladin\", \"Rogue\", \"Cleric\", \"Mage\" or \"Warrior\"")
+    class_player = input()
+    print("How would you describe yourself?")
+    description = input()
+
+    player = Player(name, race, class_player, description)
+    print("Welcome {name} !".format(name=player.name))
+    print("You are ready to go on an adventure !") 
+
+
+def entry():
+    print("Welcome to your Adventure!")
+    print("This is still a work in progress ;) ")
+    print("You can only make a player character now, but later we can go on an adventure!")
+    print("Press \"SpaceBar\" to continue...")
+    keyboard.wait("spacebar")
+    makePlayer()
+ 
+
+#The Start of the progam
+entry()
